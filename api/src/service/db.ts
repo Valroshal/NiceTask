17444d90051f375692e2419da482889c255f9a84
Valtoshal <valerie.kabishcher@gmail.com>
2023-06-07 16:44:48 +0300

    type orm config

--- api/src/service/db.ts
+++ api/src/service/db.ts
@@ -3,18 +3,6 @@ import {connectionOptions} from "../typeorm-config";
 
 async function connectToDatabase(): Promise<DataSource> {
   try {
-    // const connectionOptions: DataSourceOptions = {
-    //   type: 'postgres',
-    //   host: 'abul.db.elephantsql.com',
-    //   port: 5432,
-    //   username: 'elddwomh',
-    //   password: 'skgUevkZBiCtyZndoLUMgKK_NPDC90aD',
-    //   database: 'elddwomh',
-    //   synchronize: true,
-    //   logging: true,
-    //   entities: [User],
-    // };
-
     const dataSource: DataSource = new DataSource(connectionOptions);
     await dataSource.initialize();
     console.log('Connected to PostgreSQL');
